daa928cc2b2990a63336f5d15cf63468d72ac319
akidukisystems <admin@akidukisystems.com>
2023-08-22 17:53:32 +0900

    ディスクから読み込んでファイル内容を表示するよう修正

--- Assembler/type.asm
+++ Assembler/type.asm
@@ -4,21 +4,22 @@
     #origin addr        0
     #enum   @CRLF       0A0Dh
 
-    MOV     AH, 1
+    MOV     AH, 20h
     MOV     SI, 7AD0h
+    MOV     BX, 1700h
+    MOV     ES, BX
+    XOR     BX, BX
     INT     21h
     OR      AH, AH
     JNZ     notfound:
 
-    MOV     AH, 2
-    INT     21h
-
     PUSH    DS
-    XOR     BX, BX
-    MOV     DS, BX
-    MOV     SI, AX
+
+    MOV     DS, ES
+    MOV     SI, 0
     MOV     AH, 10h
     INT     21h
+    
     POP     DS
 
     FRET
